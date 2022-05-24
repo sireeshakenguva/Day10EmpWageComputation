83849f6eeb3c96b6a9460b4f880c77fbbc69f1a1
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-25 01:39:26 +0530

    Ability to manage Employee Wage of multiple companies using API approach

---
+++ EmpWageComputation/src/com/bridgelabz/Empwage/IEmployeeWageComputation.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.Empwage;
+
+public interface IEmployeeWageComputation {
+	
+	    public void addCompany(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs);
+
+	    public void calculateTotalWage();
+	}
+

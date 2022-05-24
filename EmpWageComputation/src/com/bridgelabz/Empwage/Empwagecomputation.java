39e153cbcd1df123e482e6eb0cdc5ee15c772a80
sireeshakenguva <sireeshakenguva@gmail.com>
2022-05-25 00:19:13 +0530

    Employee is Absent or Present

--- EmpWageComputation/src/com/bridgelabz/Empwage/Empwagecomputation.java
+++ EmpWageComputation/src/com/bridgelabz/Empwage/Empwagecomputation.java
@@ -4,5 +4,13 @@ public class Empwagecomputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EmployeeWageProblem");
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is present");
+		}
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
 }
